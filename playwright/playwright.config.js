09b77c63eeb800ef95fc6f20ed0e92d99faede7c
abhay2133 <abhaybishthestudent@gmail.com>
2026-05-04 01:33:49 +0530

    chore: add Playwright configuration

---
+++ playwright/playwright.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: { timeout: 5000 },
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 0,
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+  },
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' } },
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+    { name: 'WebKit', use: { browserName: 'webkit' } },
+  ],
+});
